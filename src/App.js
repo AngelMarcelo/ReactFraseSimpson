e7dc6b22c0f1bde42df9d27af4e9c2a6fdc11cfc
AngelMarcelo <ammarce@gmail.com>
2021-11-11 18:32:18 -0300

    se inicia nuevo proyecto simpson se modifica package json, app y se elimina src img logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {button} from 'react-bootstrap'; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='container my-5 d-flex flex-column aling-item-center'>
+      <img src={logo} alt="logo de los simpson" />
+      <button  variant='warning' className 'w-75 my-5'>Obtener frase</button>
+      <Frase></Frase>
+    </section>
   );
 }
 
