d0e434f1aad252fc663e907623a2b3a2e35db509
AngelMarcelo <ammarce@gmail.com>
2021-11-16 16:42:14 -0300

    se corrige codigo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import logo from './logo.png';
+import logo from './components/logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {button} from 'react-bootstrap'; 
+import Frase from './components/Frase';
 
 
 function App() {
   return (
     <section className='container my-5 d-flex flex-column aling-item-center'>
       <img src={logo} alt="logo de los simpson" />
-      <button  variant='warning' className 'w-75 my-5'>Obtener frase</button>
+      <button variant='warning' className 'w-75 my-5'>Obtener frase</button>
       <Frase></Frase>
     </section>
   );
